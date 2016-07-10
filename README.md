5667058bc6ff7586a5cf1db906e6f24731ec718d
Mike Gysel <mikegysel@gmail.com>
2016-07-09 18:34:49 -0700

    Finishing touches

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Map-Project
+
+## Description
+Map-Project displays the location, name, and description of various parks in New York City. 
+
+## Frameworks and Libraries
+* Knockout JS v3.4.0
+* jQuery v2.2.3
+* Bootstrap v3.3.6
+
+## API's
+The Google Maps and Foursquare API's were used for mapping and information about the park locations. See the Google Maps and Foursquare API Documentation and Terms of Use for more information.
+* Google Maps (https://developers.google.com/maps/)
+* Foursquare (https://developer.foursquare.com/)
+
+## Dependencies
+For a list of dependencies, see the package.json file.
+
+## Use
+To use the application, clone the repository and open index.html in a web browser.
--- js/map.js
+++ js/map.js
@@ -1,13 +1,141 @@
 var model = {
 	// Names and lat/longs of map locations.
-	locations: [{name: "Brooklyn Heights Promenade", position: {lat: 40.6959, lng: -73.9979}, venueID: '42377700f964a52024201fe3'},
-	{name: "Brooklyn Botanic Garden", position: {lat: 40.6689, lng: -73.9651}, venueID: '42717900f964a5206f211fe3'},
-	{name: "Brooklyn Bridge", position: {lat: 40.7061, lng: -73.9969}, venueID: '4a43bcb7f964a520bba61fe3'},
-	{name: "Golden Gate Park", position: {lat: 40.7829, lng: -73.9654}, venueID: '445e36bff964a520fb321fe3'},
-	{name: "The High Line", position: {lat: 40.7480, lng: -74.0048}, venueID: '40f1d480f964a5206a0a1fe3'}],
+	locations: [
+		{
+			name: 'Brooklyn Heights Promenade',
+			position: {lat: 40.6959, lng: -73.9979},
+			venueID: '42377700f964a52024201fe3'
+		},
+		{
+			name: 'Brooklyn Botanic Garden',
+			position: {lat: 40.6689, lng: -73.9651},
+			venueID: '42717900f964a5206f211fe3'
+		},
+		{
+			name: 'Brooklyn Bridge',
+			position: {lat: 40.7061, lng: -73.9969},
+			venueID: '4a43bcb7f964a520bba61fe3'
+		},
+		{
+			name: 'Golden Gate Park',
+			position: {lat: 40.7829, lng: -73.9654},
+			venueID: '445e36bff964a520fb321fe3'
+		},
+		{
+			name: 'The High Line',
+			position: {lat: 40.7480, lng: -74.0048},
+			venueID: '40f1d480f964a5206a0a1fe3'
+		}
+	],
+
 	foursquareconfig: {apiKey: 'NJAHHABYCD2FWFRBKTORDSHJIL3FOQBYU5E5B12HBYNCCTQN',
     					authUrl: 'https://foursquare.com/',
-    					apiUrl: 'https://api.foursquare.com/'}
+    					apiUrl: 'https://api.foursquare.com/'},
+
+    styles: [
+		    {
+		        'featureType': 'landscape',
+		        'stylers': [
+		            {
+		                'hue': '#F1FF00'
+		            },
+		            {
+		                'saturation': -27.4
+		            },
+		            {
+		                'lightness': 9.4
+		            },
+		            {
+		                'gamma': 1
+		            }
+		        ]
+		    },
+		    {
+		        'featureType': 'road.highway',
+		        'stylers': [
+		            {
+		                'hue': '#0099FF'
+		            },
+		            {
+		                'saturation': -20
+		            },
+		            {
+		                'lightness': 36.4
+		            },
+		            {
+		                'gamma': 1
+		            }
+		        ]
+		    },
+		    {
+		        'featureType': 'road.arterial',
+		        'stylers': [
+		            {
+		                'hue': '#00FF4F'
+		            },
+		            {
+		                'saturation': 0
+		            },
+		            {
+		                'lightness': 0
+		            },
+		            {
+		                'gamma': 1
+		            }
+		        ]
+		    },
+		    {
+		        'featureType': 'road.local',
+		        'stylers': [
+		            {
+		                'hue': '#FFB300'
+		            },
+		            {
+		                'saturation': -38
+		            },
+		            {
+		                'lightness': 11.2
+		            },
+		            {
+		                'gamma': 1
+		            }
+		        ]
+		    },
+		    {
+		        'featureType': 'water',
+		        'stylers': [
+		            {
+		                'hue': '#00B6FF'
+		            },
+		            {
+		                'saturation': 4.2
+		            },
+		            {
+		                'lightness': -63.4
+		            },
+		            {
+		                'gamma': 1
+		            }
+		        ]
+		    },
+		    {
+		        'featureType': 'poi',
+		        'stylers': [
+		            {
+		                'hue': '#9FFF00'
+		            },
+		            {
+		                'saturation': 0
+		            },
+		            {
+		                'lightness': 0
+		            },
+		            {
+		                'gamma': 1
+		            }
+		        ]
+		    }
+		]
 };
 
 
@@ -17,11 +145,31 @@ var viewModel = function() {
 	// For ease of use throughout ViewModel.
 	var modlength = model.locations.length;
 
+	// Create new StyledMapType object, passing through the styles data
+	// and name of the style.
+	var styledMap = new google.maps.StyledMapType(model.styles,
+		{name: 'Styled Map'});
+
 	// Create the google map object.
 	self.map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 40.713, lng: -74.010},
-		zoom: 12
+		center: {lat: 40.730, lng: -73.990},
+		zoom: 12,
+		// Add MapTypeId for the map type control.
+		mapTypeControlOptions: {
+			mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+		}
 	});
+
+	self.map.addListener('click', function() {
+		for (var i=0; i<modlength; i++) {
+			model.locations[i].marker.info.close();
+		}
+	});
+
+	// Associate the styled map with the MapTypeId
+	self.map.mapTypes.set('map_style', styledMap);
+	// Set the map to display
+	self.map.setMapTypeId('map_style');
 
 	// Foursquare API request. Upon success, create markers for each location
 	// from the model data, make them bounce and for infowindows to pop up
@@ -41,7 +189,8 @@ var viewModel = function() {
 				});
 				// Make infowindows
 				location.marker.info = new google.maps.InfoWindow({
-					content: location.name + ": " + results.response.venue.description
+					content: '<strong>'+location.name+'</strong>: ' +
+					results.response.venue.description
 				});
 				// Make each marker bounce when it is clicked.
 				location.marker.addListener('click', function() {
@@ -54,10 +203,9 @@ var viewModel = function() {
 			},
 			// Error handling
 			error: function (jqxhr, textStatus, error) {
-				document.getElementById("error").textContent = "Sorry friend, "
-				+ "there was an error grabbing data"
+				document.getElementById('error').textContent = 'Sorry friend, '
+				+ 'there was an error grabbing data'
 			}
-			// location["description"] = data.response.venue.description;
 		});
 	});
 
@@ -130,4 +278,4 @@ var viewModel = function() {
 
 };
 
-ko.applyBindings(new viewModel());
+ko.applyBindings(new viewModel())
